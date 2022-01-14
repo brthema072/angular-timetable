df24cb9220d97e636116b28c483b95ddfcf87b8a
Bruno Marthe <brunomarthe@hotmail.com>
2022-01-13 22:09:53 -0300

    Adding date by days of the week

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
 
-  headers: string[] = ["Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta=feira", "Sábado"]
+  headers: string[] = ["Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"]
+  headersDay: string[] = []
 
   dateHeader!: string;
   date: Date = new Date();;
@@ -15,15 +16,49 @@ export class CalendarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.formatDte()
+    this.formatDate()
+    this.getDayOfWeek()
   }
 
-  formatDte(){
+  formatDate(){
     this.dateHeader = (
       this.date.getDate() <= 9 ? "0" + this.date.getDate() : this.date.getDate()
     ) + "/" + (
       (this.date.getMonth() + 1) <= 9 ? "0" + (this.date.getMonth() + 1) : this.date.getMonth() + 1
     ) + "/" + this.date.getFullYear()
+  }
+
+  formatDateByParameter(day: number, month: number, year: number): string{
+    return ( day <= 9 ? "0" + day : day
+      ) + "/" + (
+        month <= 9 ? "0" + month : month
+      ) + "/" + year
+  }
+
+  getNameDayOfWeek(): string{
+    return this.headers[this.date.getDay() == 0 ? 1 : this.date.getDay() - 1]
+  }
+
+  getDayOfWeek(){
+    let day: number = 0
+
+    for(let i = 0; i < this.date.getDay(); i++){
+      day = this.date.getDate() - i
+      this.headersDay.push(this.formatDateByParameter(day, this.date.getMonth() + 1, this.date.getFullYear()))
+    }
+
+    let count: number = 0;
+    for(let i = this.date.getDay(); i < 6; i++){
+      count++;
+      day = this.date.getDate() + count
+      this.headersDay.push(this.formatDateByParameter(day, this.date.getMonth() + 1, this.date.getFullYear()))
+    }
+
+    this.headersDay = this.sortDays(this.headersDay)
+  }
+
+  sortDays(headersDay: string[]): string[]{
+    return headersDay.sort()
   }
 
 }
