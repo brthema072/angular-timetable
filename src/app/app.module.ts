6f55a0b01ebb622a95a55a4e8683f3da01e2c7c2
Bruno Marthe <brunomarthe@hotmail.com>
2022-01-29 18:27:10 -0300

    Comunicação com o firebase, registrar collection agenda e Bootstrap alert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AngularTimetableModule } from 'angular-timetable';
 
+import { environment } from '../environments/environment';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
@@ -14,9 +21,16 @@ import { CalendarComponent } from './calendar/calendar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularTimetableModule
+    AngularTimetableModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    providePerformance(() => getPerformance())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Firestore, collection, addDoc  } from '@angular/fire/firestore';
 import { TimeModel } from '../model/time';
+
 
 @Component({
   selector: 'app-calendar',
@@ -8,16 +10,43 @@ import { TimeModel } from '../model/time';
 })
 export class CalendarComponent implements OnInit {
 
+  successShowAlert: boolean = false;
+  errorShowAlert: boolean = false;
+
   startOffice: number = 8
   endOffice: number = 17
 
-  constructor() { }
+  agendaReference = collection(this.firestore, 'agenda'); 
+
+  constructor(private firestore: Firestore) { }
 
   ngOnInit(): void {
   }
 
   getClickedTime(time: TimeModel){
-    console.log(time);
+    this.addTime(time);
+  }
+
+  addTime(time: TimeModel){
+    addDoc(this.agendaReference, {
+      username: "Bruno",
+      email: "b@b.com",
+      day: time.day,
+      hour: time.hour
+    }).then((res) => {
+      this.successShowAlert = true
+
+      setTimeout(() => {
+        this.successShowAlert = false
+      }, 5000);
+    }).catch((err) => {
+      console.log(err)
+      this.errorShowAlert = true
+      
+      setTimeout(() => {
+        this.errorShowAlert = false
+      }, 5000);
+    })
   }
 
 }
